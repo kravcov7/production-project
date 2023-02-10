06cecc3f6fb3a5e938c9b054f6dcbc1a02a5c925
Artem K <a.kravtsov@mish.design>
2023-02-10 21:32:26 +0500

    Sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -12,7 +12,7 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
-      <ThemeSwitcher />
+    
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to={"/"}>
           Главная
