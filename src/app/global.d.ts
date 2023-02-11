c6afaad33305fd0631c30f10d873d041083b960e
Artem K <a.kravtsov@mish.design>
2023-02-12 01:28:16 +0500

    i18n, Define plugin

--- src/app/global.d.ts
+++ src/app/global.d.ts
@@ -14,3 +14,5 @@ declare module "*.svg" {
   const SVG: React.VFC<React.SVGProps<SVGElement>>;
   export default SVG;
 }
+
+declare const __IS_DEV__: boolean;
--- src/widgets/Navbar/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Navbar/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
+import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import cls from "./Sidebar.module.scss";
 
@@ -22,7 +23,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <button onClick={onToggle}>toggle</button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* <LangSwitcher /> */}
+        <LangSwitcher />
       </div>
     </div>
   );
